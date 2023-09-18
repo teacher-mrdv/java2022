7757301cb84fdbf13a7501336bc3202a1d67289b
teacher Marcos <teacher.mrdv@gmail.com>
2023-09-18 11:20:28 +0800

    Monday update

--- OOP/Zoologico/src/Zoo.java
+++ OOP/Zoologico/src/Zoo.java
@@ -6,12 +6,14 @@ public class Zoo
     private String name;
     private String address;
     public Animal[] animals; // aggregation
+    public int population;
 
     public Zoo(String name, String address)
     {
        this.name = name;
        this.address = address;
        this.animals = new Animal[MAXCAPACITY];
+       this.population = 0;
     }
 
     public String getName() {
@@ -35,30 +37,33 @@ public class Zoo
     }
 
     public Animal getAnimal(int index) {
-        if( animals[index] == null || index < 0 || index > MAXCAPACITY) {
+        if( index < 0 || index > MAXCAPACITY || animals[index] == null ) {
             System.out.println("Error");
             return null;
         }
         return animals[index];
     }
 
+    /* Not really useful to us...
     public void setAnimals(Animal[] animals) {
         this.animals = animals;
     }
+    */
 
-    public void setAnimal(Animal animal, int index) {
-        if( index < 0 || index > MAXCAPACITY) {
-            System.out.println("Error");
+    public void setAnimal(Animal animal) {
+        if( this.population > MAXCAPACITY) {
+            System.out.println("Over capacity");
             return;
         }
-        animals[index] = animal;
+        animals[population] = animal;
+        population++;
     }
 
     @Override
     public String toString() {
         String output =
                 "Zoo name: " + this.name + "\nAddress: " + this.address +
-                "Animals:\n";
+                "\nPopulation: " + this.population + "\nAnimals:\n";
         for(Animal a : animals) {
             if(a!=null)
                 output = output + "\t" + a.toString() + "\n";
--- OOP/Zoologico/src/ZooTest.java
+++ OOP/Zoologico/src/ZooTest.java
@@ -3,11 +3,13 @@ public class ZooTest {
 
     public static void main(String[] args) {
         Zoo ofs = new Zoo("OFS", "81 Pasir Ris Heights");
-        Animal[] ofsAnimals = new Animal[10];
-        ofsAnimals[0] = new Fish("Minmin", 2010, 'y' );
-        ofsAnimals[1] = new Zebra("Marty", 2012, "Savannah");
-        ofsAnimals[2] = new Rabbit("Arnold", 2005, "Grey");
-        ofs.setAnimals(ofsAnimals);
+        Animal ofsAnimal;
+        ofsAnimal = new Fish("Minmin", 2010, 'y' );
+        ofs.setAnimal(ofsAnimal);
+        ofsAnimal = new Zebra("Marty", 2012, "Savannah");
+        ofs.setAnimal(ofsAnimal);
+        ofsAnimal = new Rabbit("Arnold", 2005, "Grey");
+        ofs.setAnimal(ofsAnimal);
         System.out.println(ofs);
     }
 }
