3497415a57b6c0b87ae40b9b9df20cb603c885d6
teacher Marcos <teacher.mrdv@gmail.com>
2023-09-25 19:29:58 +0800

    jws-ch11
    start

---
+++ OOP/Chapter11/src/Fraction.java
@@ -0,0 +1,18 @@
+public class Fraction {
+    // attributes/fields/member or instance variables
+    // (no static/class variables needed in this class)
+    int numerator;
+    int denominator;
+
+    // constructor
+    public Fraction(int numerator, int denominator) {
+        this.denominator = denominator;
+        this.numerator = numerator;
+    }
+
+
+    // toString = polymorphism/overriding the Object class' toString method
+    public String toString() {
+        return this.numerator + "/" + this.denominator;
+    }
+}
---
+++ OOP/Chapter11/src/FractionTest.java
@@ -0,0 +1,6 @@
+public class FractionTest {
+    public static void main(String[] args) {
+        Fraction f = new Fraction(1,4);
+        System.out.println(f);
+    }
+}
