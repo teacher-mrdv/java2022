be18fb32e8daf47efa2bcb7d70c1e492263cbe35
teacher-mrdv <teacher.mrdv@gmail.com>
2023-10-02 10:01:07 +0800

    Chapter 11 answer

--- OOP/Chapter11/src/Fraction.java
+++ OOP/Chapter11/src/Fraction.java
@@ -1,22 +1,95 @@
-public class Fraction {
-    // attributes/fields/member or instance variables
-    // (no static/class variables needed in this class)
-    int numerator;
-    int denominator;
+//
+// Chapter 11, plus some extra practice for our OOP test
+//
+public class Fraction
+{
+    private int num;
+    private int den;
 
-    // constructor
-    public Fraction(int numerator, int denominator) {
-        this.denominator = denominator;
-        this.numerator = numerator;
+    public Fraction(int num, int den)
+    {
+        this.num = num;
+        this.den = den;
+        this.cancel();
+    }
+    // overloaded constructor
+    public Fraction()
+    {
+        this.num = 0;
+        this.den = 1;
     }
 
-    public Fraction add(Fraction b) {
-
+    public double toDecimal()
+    {
+        return (double)this.num / (double)this.den;
     }
 
-    // toString = polymorphism/overriding the Object class' toString method
-    public String toString() {
-        return this.numerator + "/" + this.denominator;
+    // calculate the GCD to simplify a fraction
+    private static int gcd(int n, int m)
+    {
+        int gcd;
+        if ( n == m )
+            gcd = n;
+        else
+        {
+            while (n != m)
+            {   if ( n < m )
+                m = m - n;
+            else
+                n = n - m;
+            }
+        }
+        return n;
+    }
+
+    public void cancel() // simplify
+    {
+        int gcd = gcd(this.num, this. den);
+        this.num = this.num / gcd;
+        this.den = this.den / gcd;
+    }
+
+    public int getNum()
+    {
+        return num;
+    }
+
+    public void setNum(int num)
+    {
+        this.num = num;
+    }
+
+    public int getDen()
+    {
+        return den;
+    }
+
+    public void setDen(int den)
+    {
+        this.den = den;
+    }
+
+    //  Practice for the test: write the add, equals and compareTo methods
+    public Fraction add(Fraction second)
+    {
+        int sumNum = this.num * second.den + second.num * this.den;
+        int sumDen = this.den * second.den;
+        Fraction sum = new Fraction(sumNum, sumDen);
+        return sum;
+    }
+
+    public boolean equals(Fraction second)
+    {
+        return this.toDecimal() == second.toDecimal();
+    }
+
+    public double compareTo(Fraction second)
+    {
+        return this.toDecimal() - second.toDecimal();
+    }
+
+    public String toString()
+    {
+        return num + "/" + den;
     }
 }
- 
