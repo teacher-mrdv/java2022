6a9f56ffb3868953724840c1223e08df04374caa
teacher Marcos <teacher.mrdv@gmail.com>
2023-10-17 17:44:57 +0800

    Create Test.java

---
+++ OOP/Chapter11/src/Test.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class Test {
+    public static void main(String[] args) {
+        String n = "Pepe";
+        String m = n;
+        String o = new String("Pepe");
+        String q = "Pepe";
+        System.out.println(n == m);
+        System.out.println(n == o);
+        System.out.println(n == q);
+        System.out.println(n.equals(o));
+    }
+}
