b6f4d052a923668059959cf72a594541c67edf1b
teacher Marcos <teacher.mrdv@gmail.com>
2024-01-16 19:44:49 +0800

    cleanup and added example

---
+++ IA/code_examples/other_examples/HashMapDemo.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        //ArrayList<HashMap<String, Integer>> leaderboard = new ArrayList<>();
+        HashMap<String , Integer> scores = new HashMap<String, Integer>();
+        scores.put("Peach", 7);
+        scores.put("Mario", 2);
+        scores.put("Luigi", 9);
+        scores.put("Wario", 3);
+        System.out.println(scores);
+        //System.out.println( scores. );
+        System.out.println(scores.size());
+        
+        /* Notes to Eshan:
+         * the String, int could be just a LeaderBoardData class
+         * you could add LeaderBoardData objects to an ArrayList
+         * and sort it
+         * There may be no need for the HashMaps!
+         */
+    }
+}
